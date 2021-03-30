f2d8d4b7ff710b4aaf163b7dead52e5d47398bd0
Mehdi M <mehdi@safeid.co>
2021-03-30 15:05:04 -0700

    Adde option to send funds to a non whitelisted wallet

--- lib/fireblocks/api/transactions.rb
+++ lib/fireblocks/api/transactions.rb
@@ -22,6 +22,34 @@ module Fireblocks
           Fireblocks::Request.post(body: body, path: '/v1/transactions')
         end
 
+        def from_vault_to_external_one_time(
+          amount:,
+          asset_id:,
+          source_id:,
+          one_time_address:,
+          tag: nil
+        )
+          one_time_address_hash = {
+            address: one_time_address
+          }
+          one_time_address_hash.merge(tag: tag) if tag
+
+          body = {
+            amount: amount,
+            assetId: asset_id,
+            source: {
+              type: 'VAULT_ACCOUNT',
+              id: source_id
+            },
+            destination: {
+              type: 'ONE_TIME_ADDRESS',
+              oneTimeAddress: one_time_address_hash
+            }
+          }
+          create(body)
+        end
+
+
         def from_vault_to_external(
           amount:,
           asset_id:,
